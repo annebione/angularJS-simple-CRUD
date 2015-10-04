46aa2fe2e99aead2f864d675b0088b784098df88
Anne Bione <annebione@gmail.com>
2015-10-03 22:47:16 -0300

    Update user.service.js

--- model/user.service.js
+++ model/user.service.js
@@ -4,22 +4,24 @@
 
 function UserService () {
 
-  var services = {};
+  var _services = {};
 
-  services.createUser = function ($resource) {
+  _services.createUser = function ($resource) {
     return $resource('/users', {}, {
       query: {method: 'GET', isArray: true},
       create: {method: 'POST'}
     })
   };
 
-  services.userFactory = function ($resource) {
+  _services.userFactory = function ($resource) {
     return $resource('/users/:id', {}, {
       show: {method: 'GET'},
       update: {method: 'PUT', params: {id: '@id'}},
       delete: {method: 'DELETE', params: {id: '@id'}}
     })
   };
+  
+  return _services;
 
 };
 
